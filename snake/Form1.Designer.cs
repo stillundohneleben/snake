d7e6f31aa64a65d150d50f573c8e716c0b4073e6
stillundohneleben <o.solomnikova@gmail.com>
2020-12-19 21:42:25 +0300

    first

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(104, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 121);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-2, 57);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(791, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 55);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Счёт:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(986, 691);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ snake/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Media;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace snake
+{
+    public partial class Form1 : Form
+    {
+        private int rI, rJ;
+        private PictureBox fruit;
+        private PictureBox[] snake = new PictureBox[400];
+        private int dirX, dirY;
+        private int w = 900;
+        private int h = 800;
+        private int sizeOfSides = 40;
+        private int s = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = w;
+            this.Height = h;
+            this.BackgroundImage = pictureBox1.Image;
+            dirX = 1;
+            dirY = 0;
+            fruit = new PictureBox ();
+            fruit.Image = pictureBox2.Image;
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox3.Visible = false;
+            fruit.Size = new Size(sizeOfSides, sizeOfSides);
+            snake[0] = new PictureBox();
+            snake[0].Image= pictureBox3.Image;
+            snake[0].Location = new Point(201, 201);
+            snake[0].Size = new Size(sizeOfSides - 1, sizeOfSides - 1);
+            snake[0].BackColor = Color.Red;
+            this.Controls.Add(snake[0]);
+            generateFruit();
+            timer.Tick += new EventHandler(_update);
+            timer.Interval = 200;
+            timer.Start();
+            this.KeyDown += new KeyEventHandler(dv);
+        }
+        private void checkBorders()
+        {
+            if (snake[0].Location.X < 0)
+            {
+                for (int _i = 1; _i <= s; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                s = 0;
+                label1.Text = "Счёт: " + s;
+                dirX = 1;
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream resourcestream = assembly.GetManifestResourceStream(@"snake.o.wav");
+                SoundPlayer player = new SoundPlayer(resourcestream);
+                player.Play();
+                player.PlaySync();
+            }
+            if (snake[0].Location.X > h)
+            {
+                for (int _i = 1; _i <= s; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                s = 0;
+                label1.Text = "Счёт: " + s;
+                dirX = -1;
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream resourcestream = assembly.GetManifestResourceStream(@"snake.o.wav");
+                SoundPlayer player = new SoundPlayer(resourcestream);
+                player.Play();
+                player.PlaySync();
+            }
+            if (snake[0].Location.Y < 0)
+            {
+                for (int _i = 1; _i <= s; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                s = 0;
+                label1.Text = "Счёт: " + s;
+                dirY = 1;
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream resourcestream = assembly.GetManifestResourceStream(@"snake.o.wav");
+                SoundPlayer player = new SoundPlayer(resourcestream);
+                player.Play();
+                player.PlaySync();
+            }
+            if (snake[0].Location.Y > h)
+            {
+                for (int _i = 1; _i <= s; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                s = 0;
+                label1.Text = "Счёт: " + s;
+                dirY = -1;
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream resourcestream = assembly.GetManifestResourceStream(@"snake.o.wav");
+                SoundPlayer player = new SoundPlayer(resourcestream);
+                player.Play();
+                player.PlaySync();
+            }
+        }
+        private void moveSnake()
+        {
+            for (int i = s; i >= 1; i--)
+            {
+                snake[i].Location = snake[i - 1].Location;
+            }
+            snake[0].Location = new Point(snake[0].Location.X + dirX * (sizeOfSides), snake[0].Location.Y + dirY * (sizeOfSides));
+            eatItself();
+        }
+        private void eatItself()
+        {
+            for (int _i = 1; _i < s; _i++)
+            {
+                if (snake[0].Location == snake[_i].Location)
+                {
+                    for (int _j = _i; _j <= s; _j++)
+                        this.Controls.Remove(snake[_j]);
+                    s = s - (s - _i + 1);
+                    label1.Text = "Счёт: " + s;
+                    System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                    System.IO.Stream resourcestream = assembly.GetManifestResourceStream(@"snake.o.wav");
+                    SoundPlayer player = new SoundPlayer(resourcestream);
+                    player.Play();
+                    player.PlaySync();
+                }
+            }
+        }
+        private void eatFruit()
+        {
+            if (snake[0].Location.X == rI && snake[0].Location.Y == rJ)
+            {
+                label1.Text = "Счёт: " + ++s;
+                snake[s] = new PictureBox();
+                snake[s].Image = pictureBox3.Image;
+                snake[s].Location = new Point(snake[s - 1].Location.X + 40 * dirX, snake[s - 1].Location.Y - 40 * dirY);
+                snake[s].Size = new Size(sizeOfSides - 1, sizeOfSides - 1);
+                snake[s].BackColor = Color.Red;
+                this.Controls.Add(snake[s]);
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream resourcestream = assembly.GetManifestResourceStream(@"snake.nyam.wav");
+                SoundPlayer player = new SoundPlayer(resourcestream);
+                player.Play();
+                player.PlaySync();
+                generateFruit();
+            }
+        }
+
+        private void generateFruit()
+        {
+            Random r = new Random();
+            rI = r.Next(0, h - sizeOfSides);
+            int tempI = rI % sizeOfSides;
+            rI -= tempI;
+            rJ = r.Next(0, h - sizeOfSides);
+            int tempJ = rJ % sizeOfSides;
+            rJ -= tempJ;
+            rI++;
+            rJ++;
+            fruit.Location = new Point(rI, rJ);
+            this.Controls.Add(fruit);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _update(Object myObject, EventArgs eventsArgs)
+        {
+            moveSnake();
+            eatFruit();
+            checkBorders();
+        }
+        private void dv(object sender, KeyEventArgs e)
+        { 
+
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                    dirX = 1;
+                    dirY = 0;
+                    break;
+                case "Left":
+                    dirX = -1;
+                    dirY = 0;
+                    break;
+                case "Up":
+                    dirY = -1;
+                    dirX = 0;
+                    break;
+                case "Down":
+                    dirY = 1;
+                    dirX = 0;
+                    break;
+            }
+        }
+
+    }
+}
+
